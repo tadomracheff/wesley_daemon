0e7cfa98508014c28e56b37601005e951a0a31cc
tadomracheff <tadomracheff@gmail.com>
2021-06-04 16:08:18 +0500

    add socket server

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from db.firestore import listen
 import time
+import socket
 
 listen()
 
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('localhost', 5000))
+server_socket.listen(1)
+
 while True:
-    time.sleep(100)
+    (client_socket, address) = server_socket.accept()
+    data = client_socket.recv(1)
+    time.sleep(1)
+    if not data:
+        break
+    response = '1:1 2:2;'
+    client_socket.send(str.encode(response))
+
+client_socket.close()
